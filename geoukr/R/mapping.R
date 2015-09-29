8829a64abe4448f2475ec94b45490da96fb57694
Simon Raper <simon@coppelia.io>
2015-09-29 15:21:03 +0100

    Added mapping functions to R package

--- geoukr/R/mapping.R
+++ geoukr/R/mapping.R
@@ -1,7 +1,31 @@
+#' Add data to shapes
+#'
+#' @param shapes A SpatialPolygonsDataFrame as generated using the SP package
+#' @param data The geospatial data that should be added to the shapes
+#' @param by The column on the existing dataframe that new data should be joined to
+#' @return Returns the modified shape SpatialPolygonsDataFrame
+#' @author Simon Raper
+#' @export
+
 add_data_to_shapes<-function(shapes, data, by){
   shapes@data = merge(shapes@data, data, by=by, all.x = TRUE)
   return(shapes)
 }
+
+#' Creates the data for a UK ward map by appending a ward level dataset to the ward shapes
+#'
+#' @param ward_data A ward level data set. This must contain a column called CMWD11CD that contains the ONS census merged ward codes and a column called value which contains the statistic to be mapped.
+#' @param shapes_dir The local directory containing the shape files for census merged wards and local authorities. Please see instructions on the geouk github repo for instruction on how to obtain these file. The ward shapes must be placed in a subdirectory called wardShapes and the local authority shapes in a subdirectory called laShapes.
+#' @param points TBC
+#' @return Returns an object of class uk_ward_map containing the ward_shapes with the data appended and the la_shapes
+#' @author Simon Raper
+#' @examples
+#'
+#' data(household_deprivation_3d)
+#' dep_map<-uk_ward_map(household_deprivation_3d, "/Users/simon/Documents/CodeRepos/trussell/opendata/mapShapes")
+#'
+#' @export
+
 
 uk_ward_map<-function(ward_data, shapes_dir, points = NULL) {
 
@@ -20,12 +44,26 @@ uk_ward_map<-function(ward_data, shapes_dir, points = NULL) {
 
   add_data<-add_data_to_shapes(ward_shapes, ward_data, "CMWD11CD")
 
-
-
   #Return the shapes as an object
   uk_ward_map<-list(ward_shapes = add_data, la_shapes=la_shapes, points = points)
 
+  class(uk_ward_map)<-"uk_ward_map"
+
+  return(uk_ward_map)
+
 }
+
+#' Plots a map for just one region of a uk ward map object
+#'
+#' @param region The name of the region. Possible values are: London, North West, Yorkshire and The Humber, North East, West Midlands, East Midlands, South West, East of England, South East
+#' @param uk_ward_map A UK ward map created with the uk_ward_map function
+#' @author Simon Raper
+#' @examples
+#'
+#' data(household_deprivation_3d)
+#' dep_map<-uk_ward_map(household_deprivation_3d, "/Users/simon/Documents/CodeRepos/trussell/opendata/mapShapes")
+#' ward_map_by_region("London", dep_map)
+#' @export
 
 ward_map_by_region<-function(region, uk_ward_map){
 
--- geoukr/testScripts/testFunctions.R
+++ geoukr/testScripts/testFunctions.R
@@ -1,8 +1,8 @@
 wards_to_cmw <- read.csv("~/Documents/CodeRepos/trussell/opendata/2011Census/2011CensusDownloads/geoLookups/WD11_CMWD11_LAD11_EW_LU.csv", stringsAsFactors=FALSE)
 deprivation <- read.csv("~/Documents/CodeRepos/trussell/opendata/2011Census/2011CensusDownloads/WebScraped/HouseholdsByDeprivationDimensions.csv", stringsAsFactors=FALSE)
 deprivation_cm<-merge(wards_to_cmw, deprivation, by.x = "WD11CD", by.y="geography_code")
-ward_data<-dcast(data=deprivation_cm, CMWD11CD~"value", sum, value.var = "household_deprivation_household_is_deprived_in_3_dimensions")
-dep_map<-uk_ward_map(ward_data, "/Users/simon/Documents/CodeRepos/trussell/opendata/mapShapes", points = NULL)
+household_deprivation_3d<-dcast(data=deprivation_cm, CMWD11CD~"value", sum, value.var = "household_deprivation_household_is_deprived_in_3_dimensions")
+dep_map<-uk_ward_map(ward_data, "/Users/simon/Documents/CodeRepos/trussell/opendata/mapShapes")
 shapes_dir<-"/Users/simon/Documents/CodeRepos/trussell/opendata/mapShapes"
 
 ward_map_by_region("London", dep_map)
@@ -15,5 +15,7 @@ names(administrative_geo_hierarchy)[11:12]<-c("cm_ward_code", "cm_ward_name")
 admin_geo_hierarchy<-administrative_geo_hierarchy[,c("ward_code", "ward_name", "cm_ward_code", "cm_ward_name", "local_authority_code", "local_authority_name", "county_code", "county_name", "region_code", "region_name", "country_code", "country_name")]
 
 save(admin_geo_hierarchy, file="~/Documents/CodeRepos/geouk/geoukr/data/admin_geo_hierarchy.RData")
+
+save(household_deprivation_3d, file="~/Documents/CodeRepos/geouk/geoukr/data/household_deprivation_3d.RData")
 
 length(unique(geo_hierarchy$ward_code))
